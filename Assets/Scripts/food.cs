ad8882e253bb520a6e5f048b1e3ead35873bb656
flex <flexedoyster@hotmail.com>
2014-12-17 22:12:09 -0500

    First commit
    Food moves across the screen, fatty is getting hungry

---
+++ Assets/Scripts/food.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class food : MonoBehaviour {
+	int type = 1; //0 is fatty 1 is healthy 
+	int direction; //-1 is left 1 is right
+	int speed; //speed of the object moving accross the conveyer belt
+	public Material materialFatty;
+
+	// Use this for initialization
+	void Start () {
+		//check if fatty
+		if (7 > Random.Range (0, 9)) {
+			type = 0;
+			this.renderer.material = materialFatty;
+				}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//set speed based on difficulty
+		speed = 3;
+	if (4.7 < transform.position.x) 
+		{
+			Destroy(this.gameObject);
+		}
+		transform.Translate (Vector3.right * (speed * Time.deltaTime));
+	}
+}
---
+++ Assets/Scripts/spawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+	public GameObject food;
+	public float spawnTime;
+	private float timer;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//update the timer with real time
+		timer +=Time.deltaTime;
+		//spawn when spawntime less than timer
+		if(spawnTime < timer)
+			{
+			Instantiate (food, new Vector3(-3.5f, -3f, -.5f), Quaternion.identity);
+			timer=0; //reset timer
+			}
+	}
+}
