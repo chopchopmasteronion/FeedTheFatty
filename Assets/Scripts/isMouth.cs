f770327ed24d6561bcd17c5e5c958aaba9c8d06e
SinkingShipMedia <sinkingshipmediagroup@gmail.com>
2014-12-20 13:00:39 -0500

    Score Count updates when food hits fattys mouth
    Added a rigid body collider to the food and the sphere collider(mouth).
    Also made an isMouth script that detects
    collisions between food and mouth collider. isMouth script
    also creates score gui and updates score.

---
+++ Assets/Scripts/isMouth.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class isMouth : MonoBehaviour {
+	public food food;
+	public static int score = 0; //score for the game
+	public isMouth mouth;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.name == "food(Clone)")
+		{
+			Destroy(other.gameObject);
+			updateScoreBy(+1);
+		}
+
+	}
+
+	//function that updates the score. public so other classes can call it
+	public void updateScoreBy(int deltaScore) 
+	{
+		isMouth.score += deltaScore;
+	}
+	
+	//create the gui for the game
+	void OnGUI() {
+		GUIStyle style = new GUIStyle();
+		style.fontSize = 10;
+		GUI.Label(new Rect(0,0,100,20), "Score:"+isMouth.score, style );	
+	}
+}
