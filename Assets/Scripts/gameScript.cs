7b21a72b77038e7a7f15557b351fff99872dcb0f
flex <flexedoyster@hotmail.com>
2014-12-20 20:18:02 -0500

    Revert "Revert "Moved the scoring function""
    This reverts commit e31cc1a2a136a282b13572a341b9d2024772bb6a.

---
+++ Assets/Scripts/gameScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameScript : MonoBehaviour {
+	static int score;
+	// Use this for initialization
+	void Start () {
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void updateScore()
+	{
+		score++;
+	}
+
+	//create the gui for the game
+	void OnGUI() {
+		GUIStyle style = new GUIStyle();
+		style.fontSize = 10;
+		GUI.Label(new Rect(0,0,100,20), "Score:" + score, style );	
+	}
+}
