6801dc6dcd5a82f42e15b86d55aef62ee7ede30a
flex <flexedoyster@hotmail.com>
2014-12-22 17:31:48 -0500

    Fixed the Fatty Growth Move Issue

--- Assets/Scripts/isMouth.cs
+++ Assets/Scripts/isMouth.cs
@@ -4,14 +4,22 @@ using System.Collections;
 public class isMouth : MonoBehaviour {
 	public isMouth mouth;
 	public gameScript game;
+	public int speed;
+	public float moveTime;
+	int direction;
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+{
+	moveTime=0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		gameScript gameDiff = (gameScript)GameObject.Find("Main Camera").GetComponent(typeof (gameScript));
+		if(gameDiff.difficulty > 0)
+		{
+			sendMove();
+		}
 	}
 
 	void OnTriggerEnter(Collider other)
@@ -29,6 +37,44 @@ public class isMouth : MonoBehaviour {
 			}
 			Destroy(other.gameObject);
 
+		}
+	}
+	void move(int direction)
+	{
+		switch (direction)
+		{
+		case 0: 
+			print ("left");
+			transform.Translate (Vector3.left * (speed * Time.deltaTime));
+			break;
+		case 1:	
+			print ("right");
+			transform.Translate (Vector3.right * (speed * Time.deltaTime));
+			break;
+		}
+	}
+	
+	void sendMove()
+	{
+		if(moveTime > 0 && (transform.position.x > -7 && transform.position.x < 7))	
+		{
+			move (direction);
+			moveTime -= Time.deltaTime;
+		}
+		else if (transform.position.x < -7)
+		{
+			move (1);
+			direction = 1;
+		}
+		else if (transform.position.x > 7)
+		{
+			move (0);
+			direction = 0;
+		}
+		else 
+		{
+			moveTime = Random.Range(1,10);
+			direction = Random.Range(0,2);	
 		}
 	}
 }
