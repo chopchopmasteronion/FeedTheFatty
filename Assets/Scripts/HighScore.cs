4e91714f20137ead9a9cbce686ba4d66ac821f18
flex <flexedoyster@hotmail.com>
2015-03-28 12:56:00 -0400

    High Score Page and High Score Menu Button
    Added a high score page and a menu button to link to the animation.
    Click on screen to return to home page.

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HighScore : MonoBehaviour {
+	persistence ps;
+	public Text hs;
+	public GameObject food;
+	public GameObject topClone; //top clone of our instantiated prefab food
+	public GameObject bottomClone; //bottom clone of our instantiated prefab food
+	private float timer;
+	// Use this for initialization
+	void Start () {
+	ps = new persistence();
+	hs.text = ps.writeScore();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if(.5 < timer)
+		{
+			topClone = Instantiate (food, new Vector3(10f, -3.1f, -.5f), Quaternion.identity) as GameObject;
+			topClone.tag = "Top"; //tag the food top so we can identify it later
+			bottomClone = Instantiate (food, new Vector3(-10f, -4.1f, -.5f), Quaternion.identity) as GameObject;
+			bottomClone.tag = "Bottom"; 
+			bottomClone = Instantiate (food, new Vector3(-10f, -2.1f, -.5f), Quaternion.identity) as GameObject;
+			bottomClone.tag = "Bottom"; 
+			topClone = Instantiate (food, new Vector3(10f, -1.1f, -.5f), Quaternion.identity) as GameObject;
+			topClone.tag = "Top"; //tag the food top so we can identify it later
+			bottomClone = Instantiate (food, new Vector3(-10f, -0.1f, -.5f), Quaternion.identity) as GameObject;
+			bottomClone.tag = "Bottom"; 
+			topClone = Instantiate (food, new Vector3(10f, 1.1f, -.5f), Quaternion.identity) as GameObject;
+			topClone.tag = "Top"; //tag the food top so we can identify it later
+			bottomClone = Instantiate (food, new Vector3(-10f, 2.1f, -.5f), Quaternion.identity) as GameObject;
+			bottomClone.tag = "Bottom"; 
+			topClone = Instantiate (food, new Vector3(10f, 3.1f, -.5f), Quaternion.identity) as GameObject;
+			topClone.tag = "Top"; //tag the food top so we can identify it later
+			bottomClone = Instantiate (food, new Vector3(-10f, 4.1f, -.5f), Quaternion.identity) as GameObject;
+			bottomClone.tag = "Bottom"; 
+
+			timer = 0;
+		}
+		if (Input.anyKeyDown) {
+			Application.LoadLevel("Menu");
+		}
+	}
+	public int getDifficulty()
+	{
+		return 3;
+	}	
+}
--- Assets/Scripts/menuFood.cs
+++ Assets/Scripts/menuFood.cs
@@ -1,17 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class food : MonoBehaviour {
+public class menuFood : MonoBehaviour {
+
 	int type = 1; //0 is fatty 1 is healthy 
 	int direction; //-1 is left 1 is right
-	int touched = 0; //0 is not touched, 1 is touched
 	int weight;
 	public int speed; //speed of the object moving accross the conveyer belt
-	public int launchSpeed; //speed when launched
 	public Sprite[] fatFoods;
-	public AudioClip splat;
-
-
+	
+	
 	// Use this for initialization
 	void Start () {
 		weight = 0;
@@ -27,44 +25,22 @@ public class food : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if (touched == 0) 
-		{	
-			//if the tag is top then ride to the right
-			if(this.gameObject.tag == "Top")
-			{
-				Ride (1);
-			}
-			else
-			{
-				//if tag is not top ride to the right
-				Ride (0);
-			}
-		}
-		else 
+		//if the tag is top then ride to the right
+		if(this.gameObject.tag == "Top")
 		{
-			//if tag is top then launch down
-			if(this.gameObject.tag == "Top")
-			{
-				Launch(1);
-			}
-			else
-			{
-				//if tag is not top then launch up
-				Launch (0);
-			}
+			Ride (1);
+		}
+		else
+		{
+			//if tag is not top ride to the right
+			Ride (0);
 		}
 	}
 
-	//Touch is called from the control class
-	void Touch()
-	{
-		touched = 1;
-	}
-
+	
 	//Ride provides a means of transport for food
 	void Ride(int type)
 	{
-		gameScript gameDiff = (gameScript)GameObject.Find("Main Camera").GetComponent(typeof (gameScript));
 		//move bottom foods to the right
 		if(type == 0)
 		{
@@ -73,7 +49,7 @@ public class food : MonoBehaviour {
 			{
 				Destroy(this.gameObject);
 			}
-			transform.Translate (Vector3.right * ((gameDiff.getDifficulty() + speed) * Time.deltaTime));
+			transform.Translate (Vector3.right * 6 * Time.deltaTime);
 			
 		}
 		//move top foods to the left
@@ -84,58 +60,15 @@ public class food : MonoBehaviour {
 			{
 				Destroy(this.gameObject);
 			}
-			transform.Translate (Vector3.left * ((gameDiff.getDifficulty() + speed) * Time.deltaTime));
+			transform.Translate (Vector3.left * 6 * Time.deltaTime);
 		}
 	}
 
-	//Launch sends the food towards the center
-	void Launch(int type)
-	{
-		//tag is bottom so we need to launch up
-		if(type == 0)
-		{
-			if (0 < transform.position.y) 
-			{
-				this.destroy();
-				
-			}
-			transform.Translate(Vector3.up * (launchSpeed * Time.deltaTime), Space.World);
-			transform.Rotate(0, 0, 360 * Time.deltaTime);
-		}
-		//tag is top so we need to launch down
-		if(type == 1)
-		{
-			if (0 > transform.position.y) 
-			{
-				this.destroy();
-			}
-			transform.Translate (Vector3.down * (launchSpeed * Time.deltaTime), Space.World);
-			transform.Rotate(0, 0, -360 * Time.deltaTime);
-		}
-	}
-	
-	void destroy()
-	{
-		audio.PlayOneShot(splat);
-		Destroy(this.gameObject);
-	}
-
-	//returns the type of food
-	public int getType()
-	{
-		return type;
-	}
-	
-	public int getWeight()
-	{
-		return weight;
-	}
-	
 	void SetWeight(int choice)
 	{
 		if(choice == 1)
 		{
-
+			
 			weight = (Random.Range(1,8));
 			switch (weight)
 			{
@@ -184,6 +117,6 @@ public class food : MonoBehaviour {
 			}
 		}
 		
-
+		
 	}
 }
